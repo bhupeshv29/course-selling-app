4a31a7ce2eb0b99b537bd956e3df99d170820606
bhupesh <bhupeshverma29bv@gmail.com>
2025-03-25 16:44:47 +0530

    keep alive bot added

--- course_selling_Api/index.js
+++ course_selling_Api/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
+import fetch from 'node-fetch';
 
-// console.log(process.env.Mongo_url);
 import express from 'express'
 import mongoose from 'mongoose'
 import { userRouter } from './routes/user.js';
@@ -11,22 +11,23 @@ import cors from 'cors'
 
 const app  = express()
 
-
-
 app.use(cors())
 app.use(express.json())
-
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-
-
 async function main() {
     await mongoose.connect(process.env.MONGO_URL);    
     app.listen(3000);
-    console.log("listening on port 3000")
+    console.log("listening on port 3000");
+
+    setInterval(() => {
+        fetch('https://course-selling-app-woad.vercel.app/')
+            .then(res => console.log('Keep-alive ping successful'))
+            .catch(err => console.error('Keep-alive ping failed:', err));
+    }, 5 * 60 * 1000); // Ping every 5 minutes
 }
 
 main()
